b3d930fe1db2f7fee6a146c3f119683757e2e99a
anson <ansonsuen32@gmail.com>
2021-08-22 13:46:40 +0800

    upload scarpe and run code

---
+++ collecting data.py
@@ -0,0 +1,11 @@
+from lxml.html.builder import A
+from pandas import DataFrame
+from selenium import webdriver
+import pandas as pd
+from bs4 import BeautifulSoup
+import glassdoor_scraper as gs
+import numpy as np
+url='https://www.nba.com/players'
+df =pd.read_html(url,header=0)
+df=pd.DataFrame(np.concatenate(df),columns=['Player','Team','Numbers','Position','Height','Weight','Last_Attedent','Country'])
+df=df.to_csv('NBA.csv')
